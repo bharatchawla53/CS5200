930ba618b6b0c5325c0e8fe8a043c75bafe299fe
Bharat Chawla <35283839+bharatchawla53@users.noreply.github.com>
2022-09-20 23:21:23 -0400

    hw3

--- src/CS-5200/hw3/hwk3problem2and5chawlab.sql
+++ src/CS-5200/hw3/hwk3problem2and5chawlab.sql
@@ -4,4 +4,15 @@
 --     2. ex database and tables that are created under it are: null_sample, departments, employees, projects,
 --        customers, color_sample, string_sample, float_sample, date_sample, active_invoices, paid_invoices.
 --     3. om database and tables that are created under it are: customers, items, orders, order_details.
---
+-- 2c) 68 records were inserted into the om.order_details by the script.
+-- 2d) 114 records were inserted into the ap.invoices by the script.
+-- 2e) 122 records were inserted into the ap.vendors by the script.
+-- 2f) Yes, there is a foreign key between the ap.invoices and the ap.vendors table.
+-- 2g) ap.vendors has 2 foreign keys to it.
+-- 2h) customer_id is the primary key for the om.customers table.
+-- 2i) Write a SQL command that will retrieve all values for all fields from the orders table
+       select *
+       from om.orders
+-- 2j) Write a SQL command that will retrieve the fields: title and artist from the om.items table
+       select title, artist
+       from om.items
