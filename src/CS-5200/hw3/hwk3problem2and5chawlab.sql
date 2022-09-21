171b990f2a39c97bbdaa71306a03da6dada978ca
Bharat Chawla <35283839+bharatchawla53@users.noreply.github.com>
2022-09-20 23:08:14 -0400

    hw3

---
+++ src/CS-5200/hw3/hwk3problem2and5chawlab.sql
@@ -0,0 +1,7 @@
+-- 2a) 3 databases are created by the script.
+-- 2b) 1. ap database and tables that are created under it are: general_ledger_accounts, terms, vendors,
+--        invoices, invoice_line_items, vendor_contacts, invoice_archive.
+--     2. ex database and tables that are created under it are: null_sample, departments, employees, projects,
+--        customers, color_sample, string_sample, float_sample, date_sample, active_invoices, paid_invoices.
+--     3. om database and tables that are created under it are: customers, items, orders, order_details.
+--
